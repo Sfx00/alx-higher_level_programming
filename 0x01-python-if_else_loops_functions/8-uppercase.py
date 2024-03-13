c919a3680e6bd9245bdf8a1baedd17b929f4d45b
Oussama Bajali <140127359+Sfx00@users.noreply.github.com>
2024-03-13 01:41:44 +0000

    snow

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    """Print a string in uppercase."""
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print("")
